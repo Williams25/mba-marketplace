4e9cfb61ff742622a3047ac3fda1177f8ed1ae24
Williams25 <william.souza.timao@hotmail.com>
2024-10-08 20:03:28 -0300

    criando componente de detalhes de loja

---
+++ src/components/StoreProfileDialog.tsx
@@ -0,0 +1,57 @@
+import { useQueryClient } from "@tanstack/react-query";
+import {
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle
+} from "./ui/dialog";
+import { ISellersResponse } from "@/types/sellers";
+import { QUERY_KEYS } from "@/constants/QueryKeys";
+import { Skeleton } from "./ui/skeleton";
+
+export const StoreProfileDialog = () => {
+  const queryClient = useQueryClient();
+  const cached = queryClient.getQueryData<ISellersResponse>([
+    QUERY_KEYS.GET_PROFILE
+  ]);
+  return (
+    <DialogContent>
+      <DialogHeader>
+        <DialogTitle>Perfil da loja</DialogTitle>
+        <DialogDescription>
+          Informações do seu estabelecimento visíveis ao seu cliente
+        </DialogDescription>
+      </DialogHeader>
+
+      <div className="flex gap-6 items-start">
+        {cached ? (
+          <img
+            src={cached?.seller.avatar.url}
+            className="w-28 h-28 rounded-full object-cover"
+          />
+        ) : (
+          <Skeleton className="w-28 h-28 min-w-28 min-h-28 rounded-full" />
+        )}
+        {cached ? (
+          <div className="w-full flex flex-col">
+            <span className="font-bold text-lg text-gray-700">
+              {cached?.seller.name}
+            </span>
+            <span className="font-medium text-sm text-gray-600">
+              {cached?.seller.email}
+            </span>
+            <span className="font-medium text-sm text-gray-600">
+              {cached?.seller.phone}
+            </span>
+          </div>
+        ) : (
+          <div className="w-full flex flex-col">
+            <Skeleton className="mt-2 h-4 w-40" />
+            <Skeleton className="mt-2 h-4 w-40" />
+            <Skeleton className="mt-2 h-4 w-40" />
+          </div>
+        )}
+      </div>
+    </DialogContent>
+  );
+};
