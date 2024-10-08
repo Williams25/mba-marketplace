f745caf728976d8870d3f04bbaefd93184bfb430
Williams25 <william.souza.timao@hotmail.com>
2024-10-08 20:02:48 -0300

    criando estilo do default layout

--- src/_layouts/DefaultLayout.tsx
+++ src/_layouts/DefaultLayout.tsx
@@ -3,6 +3,11 @@ import { STORAGE_KEYS } from "@/constants/StorageKeys";
 import { getCookie } from "@/utils/cookie";
 import { useEffect } from "react";
 import { Outlet, useNavigate } from "react-router-dom";
+import LogoHeaderImg from "../assets/logo-header.svg";
+import { NavLink } from "@/components/NavLink";
+import { ChartNoAxesCombined, Plus, ShoppingBag } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { UserSettings } from "@/components/UserSettings";
 
 export const DefaultLayout = () => {
   const navigate = useNavigate();
@@ -13,11 +18,34 @@ export const DefaultLayout = () => {
   }, []);
 
   return (
-    <div>
-      DefaultLayout
-      <div>
+    <>
+      <div className="border-2 border-b-gray-200 mb-12">
+        <header className="py-5 px-6 max-w-[1720px] w-full mx-auto flex justify-between items-center gap-4">
+          <img src={LogoHeaderImg} className="w-14 h-10" />
+          <nav className="flex items-center space-x-4 lg:space-x-6">
+            <NavLink to={DEFAULT_ROUTES.PRIVATE.HOME}>
+              <ChartNoAxesCombined className="w-5 h-5" />
+              Dashboard
+            </NavLink>
+            <NavLink to={"/orders"}>
+              <ShoppingBag className="w-5 h-5" />
+              Produtos
+            </NavLink>
+          </nav>
+
+          <div className="flex items-center space-x-4 lg:space-x-6">
+            <Button className="flex items-center gap-2 font-medium">
+              <Plus className="w-5 h-5" />
+              Novo Produto
+            </Button>
+
+            <UserSettings />
+          </div>
+        </header>
+      </div>
+      <div className="max-w-[1400px] w-full mx-auto">
         <Outlet />
       </div>
-    </div>
+    </>
   );
 };
