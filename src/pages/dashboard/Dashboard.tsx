49f233f7e43ae372cceddda670c62dcdb691dc18
Williams25 <william.souza.timao@hotmail.com>
2024-10-09 20:37:24 -0300

    feat: finalizando dashboard

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,26 @@
 import { Metrics } from "@/components/Metrics";
+import { VisitorsGraph } from "@/components/VisitorsGraph";
+import { QUERY_KEYS } from "@/constants/QueryKeys";
+import { metricsService } from "@/services/metricsService";
+import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 
 export const Dashboard = () => {
+  const { data: sold, isLoading: isLoadingSold } = useQuery({
+    queryKey: [QUERY_KEYS.GET_SOLD],
+    queryFn: metricsService.sold
+  });
+
+  const { data: available, isLoading: isLoadingAvailable } = useQuery({
+    queryKey: [QUERY_KEYS.GET_AVAILABLE],
+    queryFn: metricsService.available
+  });
+
+  const { data: views, isLoading: isLoadingViews } = useQuery({
+    queryKey: [QUERY_KEYS.GET_VIEWS],
+    queryFn: metricsService.views
+  });
+
   return (
     <>
       <Helmet title="Dashboard" />
@@ -15,12 +34,27 @@ export const Dashboard = () => {
 
       <div className="grid grid-flow-col grid-cols-[240px_auto] gap-10">
         <div className="flex flex-col gap-4">
-          <Metrics label="Produtos vendidos" total="25" type="sold" />
-          <Metrics label="Produtos anunciados" total="56" type="store" />
-          <Metrics label="Pessoas visitantes" total="1.238" type="visitors" />
+          <Metrics
+            label="Produtos vendidos"
+            total={String(sold?.amount || 0)}
+            type="sold"
+            isPending={isLoadingSold}
+          />
+          <Metrics
+            label="Produtos anunciados"
+            total={String(available?.amount || 0)}
+            type="store"
+            isPending={isLoadingAvailable}
+          />
+          <Metrics
+            label="Pessoas visitantes"
+            total={String(views?.amount || 0)}
+            type="visitors"
+            isPending={isLoadingViews}
+          />
         </div>
 
-        <div>grafico</div>
+        <VisitorsGraph />
       </div>
     </>
   );
