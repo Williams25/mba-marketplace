638af8069de37bbe9483901eae8eb2299fe048eb
Williams25 <william.souza.timao@hotmail.com>
2024-10-10 21:03:53 -0300

    criando pagina de novo produto

---
+++ src/pages/dashboard/NewProduct.tsx
@@ -0,0 +1,19 @@
+import { FormProduct } from "@/components/FormProduct";
+import { Helmet } from "react-helmet-async";
+
+export const NewProduct = () => {
+  return (
+    <>
+      <Helmet title="Novo Produto" />
+
+      <div className="mb-12">
+        <h1 className="font-bold text-2xl">Novo produto</h1>
+        <span className="font-semibold text-base text-gray-400">
+          Cadastre um produto para venda no marketplace
+        </span>
+      </div>
+
+      <FormProduct status="sold" />
+    </>
+  );
+};
