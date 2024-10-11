ca0f799e769df8a871619169b09e3c124cf277e0
Williams25 <william.souza.timao@hotmail.com>
2024-10-11 16:28:49 -0300

    fix: card de produtos

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -11,6 +11,9 @@ import {
   CardContent
 } from "./ui/card";
 import { currencyMask } from "@/utils/masks";
+import { Link } from "react-router-dom";
+import { DEFAULT_ROUTES } from "@/constants/DefaultRoutes";
+import { parsePriceDividedHundred } from "@/utils/currencyUtils";
 
 export const ProductCard = ({
   attachments,
@@ -18,39 +21,43 @@ export const ProductCard = ({
   title,
   priceInCents,
   category,
-  status
+  status,
+  id
 }: IProduct) => {
   return (
-    <Card className="w-80">
-      <CardHeader className="p-2 relative">
-        <div className="absolute top-6 right-3 flex gap-2">
-          <span
-            className={`px-3 py-1 rounded-full text-sm font-semibold ${DefaultStatusColor[status] || "bg-gray-800 text-gray-200"}`}
-          >
-            {IProductTranslate[status]}
-          </span>
-          <span className="px-3 py-1 bg-gray-800 text-gray-200  rounded-full text-sm font-semibold">
-            {category.title}
-          </span>
-        </div>
-        <img
-          src={attachments[0].url}
-          alt={description}
-          className="w-80 h-36 rounded-md object-cover"
-        />
+    <Link to={DEFAULT_ROUTES.PRIVATE.PRODUCT_DETAILS.replace(":id", id)}>
+      <Card className="w-80">
+        <CardHeader className="p-2 relative">
+          <div className="absolute top-6 right-3 flex gap-2">
+            <span
+              className={`px-3 py-1 rounded-full text-sm font-semibold ${DefaultStatusColor[status] || "bg-gray-800 text-gray-200"}`}
+            >
+              {IProductTranslate[status]}
+            </span>
+            <span className="px-3 py-1 bg-gray-800 text-gray-200  rounded-full text-sm font-semibold">
+              {category.title}
+            </span>
+          </div>
+          <img
+            src={attachments[0].url}
+            alt={description}
+            className="w-80 h-36 rounded-md object-cover"
+          />
 
-        <CardTitle className="flex justify-between items-end px-4 pt-3 font-semibold text-lg text-gray-800">
-          {title}{" "}
-          <span className="flex gap-3">
-            <small>R$</small> {currencyMask(priceInCents / 100)}
-          </span>
-        </CardTitle>
-      </CardHeader>
-      <CardContent>
-        <CardDescription className="font-medium text-gray-500 text-base leading-5">
-          {description}
-        </CardDescription>
-      </CardContent>
-    </Card>
+          <CardTitle className="flex justify-between items-end px-4 pt-3 font-semibold text-lg text-gray-800">
+            {title}{" "}
+            <span className="flex gap-3">
+              <small>R$</small>{" "}
+              {currencyMask(parsePriceDividedHundred(priceInCents))}
+            </span>
+          </CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CardDescription className="font-medium text-gray-500 text-base leading-5">
+            {description}
+          </CardDescription>
+        </CardContent>
+      </Card>
+    </Link>
   );
 };
